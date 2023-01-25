64d490d22f56a6e27faaac9431338823325e42cd
Ladi_Cydeo <ladikhachidze@gmail.com>
2023-01-25 16:58:57 -0500

    lambda expressions Full execution

---
+++ m3-lambda/src/main/java/com/cydeo/AppleTest.java
@@ -0,0 +1,43 @@
+package com.cydeo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AppleTest {
+
+    public static void main(String[] args) {
+
+
+
+
+
+        List<Apple> inventory = new ArrayList<Apple>();
+
+        inventory.add(new Apple(100,Color.GREEN));
+        inventory.add(new Apple(300,Color.RED));
+        inventory.add(new Apple(178,Color.GREEN));
+        inventory.add(new Apple(321,Color.RED));
+
+
+        List<Apple> heavyApple = filterApples(inventory,new AppleHeavyPredicate());
+        System.out.println(heavyApple);
+        List<Apple> greenApple = filterApples(inventory,new AppleGreenColorPredicate());
+        System.out.println(greenApple);
+
+
+    }
+
+
+    private static List<Apple> filterApples(List<Apple> inventory, ApplePredicate applePredicate){
+
+        List<Apple> result = new ArrayList<>();
+        for (Apple apple : inventory){
+            if(applePredicate.test(apple)){
+                result.add(apple);
+            }
+        }
+        return result;
+
+    }
+
+}
