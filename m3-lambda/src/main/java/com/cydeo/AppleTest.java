846c98af6bd5521d2e0d6d84b38882880972e390
Ladi_Cydeo <ladikhachidze@gmail.com>
2023-01-26 15:13:18 -0500

    lambda expressions Apple test / sorting method

--- m3-lambda/src/main/java/com/cydeo/AppleTest.java
+++ m3-lambda/src/main/java/com/cydeo/AppleTest.java
@@ -25,6 +25,9 @@ public class AppleTest {
         System.out.println(greenApple);
 
 
+        ApplePredicate weightApple = apple -> apple.getWeight()>200;
+        filterApples(inventory,weightApple);
+
     }
 
 
--- m3-lambda/src/main/java/com/cydeo/Sorting.java
+++ m3-lambda/src/main/java/com/cydeo/Sorting.java
@@ -4,4 +4,8 @@ public interface Sorting {
 
     public void sort();
 
+
+
+
+
 }
