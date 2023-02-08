0fb9eedfad8c3e9f5b048a08bc19b894252f34c2
Ladi_Cydeo <ladikhachidze@gmail.com>
2023-02-08 13:19:54 -0500

    java13 TextBlockDemo

--- m8-new-version-updates/src/main/java/com/cydeo/java12/TeengDemo.java
+++ m8-new-version-updates/src/main/java/com/cydeo/java12/TeengDemo.java
@@ -1,10 +1,33 @@
 package com.cydeo.java12;
 
+import java.util.*;
+import java.util.stream.Collectors;
+
 public class TeengDemo {
 
     public static void main(String[] args) {
 
 
+        List<Employee> employeeList = Arrays.asList(
+                new Employee(1,"A",100),
+                new Employee(2,"B",200),
+                new Employee(3,"C",300),
+                new Employee(4,"D",400)
+        );
+
+
+        HashMap<String,Employee> result = employeeList.stream().collect(Collectors.teeing(
+                Collectors.maxBy(Comparator.comparing(Employee::getSalary)),
+                Collectors.minBy(Comparator.comparing(Employee::getSalary)),
+                (e1,e2) -> {
+                    HashMap<String,Employee> map = new HashMap<>();
+                    map.put("Max",e1.get());
+                    map.put("Min",e2.get());
+                    return map;
+                }
+        ));
+
+        System.out.println(result);
 
 
     }
---
+++ m8-new-version-updates/src/main/java/com/cydeo/java13/TextBlockDemo.java
@@ -0,0 +1,22 @@
+package com.cydeo.java13;
+
+public class TextBlockDemo {
+
+    public static void main(String[] args) {
+
+        var address= "5757 Dow ave \n" + " Alexandria, VA 22304";
+
+
+        var address2 = """
+                
+                5757 Dow ave
+                Alexandria, Va
+                zip: 22304
+                
+                """;
+
+
+    }
+
+
+}
