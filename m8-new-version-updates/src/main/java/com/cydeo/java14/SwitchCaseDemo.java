b8e8b32de01d0155e083c4f133aa444d9a15c8be
Ladi_Cydeo <ladikhachidze@gmail.com>
2023-02-08 13:33:57 -0500

    java14 update futures Switch case change explained / yield

--- m8-new-version-updates/src/main/java/com/cydeo/java14/SwitchCaseDemo.java
+++ m8-new-version-updates/src/main/java/com/cydeo/java14/SwitchCaseDemo.java
@@ -1,5 +1,7 @@
 package com.cydeo.java14;
 
+import java.util.Scanner;
+
 public class SwitchCaseDemo {
     public static void main(String[] args) {
 
@@ -36,6 +38,25 @@ public class SwitchCaseDemo {
             default -> 0;
         };
 
+        System.out.println(day1);
+
+
+        int day2 = switch (month){
+            case 1,3,5,7,8,10,12 -> 31;
+            case 4,6,9 -> 30;
+            case 2 -> {
+                Scanner scanner = new Scanner(System.in);
+                System.out.println("enter year");
+                int year = scanner.nextInt();
+                if(year%4 == 0)
+                    yield 29;
+                else
+                    yield 28;
+            }
+            default -> 0;
+        };
+
+        System.out.println(day2);
 
     }
 }
