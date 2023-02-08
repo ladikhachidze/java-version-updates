4ba543fc9fc0f9715da4274245c94b2e80c7bbe6
Ladi_Cydeo <ladikhachidze@gmail.com>
2023-02-08 13:45:39 -0500

    java14 update futures Record class

---
+++ m8-new-version-updates/src/main/java/com/cydeo/java14/User.java
@@ -0,0 +1,6 @@
+package com.cydeo.java14;
+
+public record User(String name,String address) {
+    //ALL ARG CONST
+
+}
---
+++ m8-new-version-updates/src/main/java/com/cydeo/java14/UserTest.java
@@ -0,0 +1,12 @@
+package com.cydeo.java14;
+
+public class UserTest {
+    public static void main(String[] args) {
+
+        User user = new User("Vlad","dow ave");
+        System.out.println(user.name());
+        System.out.println(user.toString());
+
+
+    }
+}
